791ee9cec08af116eb4838163e76bf44bd9288b7
Ozan <61967732+ozanyanak@users.noreply.github.com>
2023-07-07 18:33:56 +0300

    .

--- rental-service/src/main/java/com/kodlamaio/rentalservice/RentalServiceApplication.java
+++ rental-service/src/main/java/com/kodlamaio/rentalservice/RentalServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RentalServiceApplication {
-
+//deneme
 	public static void main(String[] args) {
 		SpringApplication.run(RentalServiceApplication.class, args);
 	}
